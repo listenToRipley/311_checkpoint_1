a881b5dc46b72643c14740ac3e668bfdeb72246b
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-06-16 08:34:01 -0500

    update the put

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const users = require('../data/index')
+const { find } = require('../data/index')
 
 // GET
 const listUsers = (req, res) => res.json(users)
@@ -22,7 +23,7 @@ const showUser = (req, res) => {
 
 //POST
 const createUsers = (req, res) => {
-  console.log('you can see creation attempt')
+  // console.log('you can see creation attempt')
   const count = users.length
   const input = req.body
   console.log('count', count, 'input', input)
@@ -57,7 +58,6 @@ const createUsers = (req, res) => {
  }
 //the end of the company nested object
 }
-//the end of the whole object 
 
 if(!createEntry.name || !createEntry.username || !createEntry.email) {
   return res.status(404).json({ msg: 'something went wrong'})
@@ -65,10 +65,10 @@ if(!createEntry.name || !createEntry.username || !createEntry.email) {
 //add the info the your doc
 users.push(createEntry)
 
+//the end of the whole object 
 return res.json(users)
 
  }
-
 
  //PUT
 const updateUser = (req, res) => {
@@ -85,13 +85,36 @@ const updateUser = (req, res) => {
   const update = req.body
 
   if(finder(users, id) === true) {
-    console.log(finder)
+  finder.id = update.id ? update.id : users.id,
+  finder.name = update.name ? update.name : users.name,
+  finder.username = update.username ? update.username : users.username,
+  finder.email = update.email ? update.email : users.email,
+//first nested
+ finder.address = {
+    street = update.address.street ? update.address.street : users.address.street,
+    suite = update.address.suite ? update.address.suite : users.address.suite,
+    city = update.address.city ? update.address.city : users.address.city,
+    zipcode = update.address.zipcode ? update.address.zipcode : users.address.zipcode,
+//address nested 
+   geo = {
+     lat =  update.address.geo.lat ? update.address.geo.lat : users.address.geo.lat,
+     lng = update.address.geo.lng ? update.address.geo.lng: users.address.geo.lng,
+   }
+// in end of the address nested object 
+ },
+//second next
+finder.phone = update.phone ? update.phone : users.phone,
+finder.website = update.website ? update.website : users.website
+//nested in the main
+find.company = {
+   name = update.name ? update.name : users.name,
+   catchPhrase = update.catchPhrase ? update.catchPhrase : users.catchPhrase,
+   bs = update.bs ? update.bs : users.bs,
     //need to write some kind of logic to look through all the keys and write a function to have a ternary operator to replace the key 
   }
-
   //error handling 
  }
-
+}
 
 //DELETE
 const deleteUser = (req, res) => {
